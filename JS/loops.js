397742001689eea0e09fd7a58fd7c28bddfa021f
thulasibatchu <bthulasi2552002@gmail.com>
2025-07-10 10:06:12 +0530

    objects

--- JS/loops.js
+++ JS/loops.js
@@ -20,7 +20,20 @@
 // }while(i<=10)
 
 //for of
-let nums=[1,2,3,4,5,6,7,8,9,10]
-for(let num of nums){
-    console.log(num)
+// let nums=[1,2,3,4,5,6,7,8,9,10]
+// for(let num of nums){
+//     console.log(num)
+// }
+
+//for-in (used only for objects)
+
+let emp={
+    eid:101,
+    ename:'rahul',
+    gender:'Male'
+}
+
+for(prop in emp){
+    // console.log(emp.eid)
+    console.log(emp[prop])
 }
---
+++ JS/objects.js
@@ -0,0 +1,29 @@
+//to verify array is empty
+
+// let users=[1,2,3]
+// if(users.length>0){
+//     console.log("Not empty array")
+// }
+// else{
+//     console.log("Empty array")
+//}
+
+//to check object is empty
+
+let emp={
+    eid:101,
+    ename:'rahul',
+    gender:'Male'
+}
+// console.log(Object.keys(emp))
+// console.log(Object.keys(emp).length>0)
+
+//using ternary operator
+
+// Object.keys(emp).length>0 ? console.log('not empty') : console.log('empty')
+
+
+
+
+
+
