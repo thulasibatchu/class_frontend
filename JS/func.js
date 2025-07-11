e4f32a97f38e9638ce42d4d815a1c8a5f22f5927
thulasibatchu <bthulasi2552002@gmail.com>
2025-07-11 15:42:02 +0530

    funcjs

---
+++ JS/func.js
@@ -0,0 +1,25 @@
+// function add(a,b){
+//     console.log(a+b)
+// }
+// add(1,2)
+
+// function add(a,b,c=1){
+//     console.log(a+b+c)
+// }
+
+// add(1,2)
+
+// function add(a,b){
+//     let c=100
+//     return a+b+c
+// }
+// let r1=add(10,20)
+// console.log(r1)
+
+function add(a,b){
+    let c=100
+    return a+b+c
+    console.log("good morning")
+}
+let r1=add(10,20)
+console.log(r1)
