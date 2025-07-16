871ca89e5fc5b46d9f648a8af13ee9b5f1496e1a
thulasibatchu <bthulasi2552002@gmail.com>
2025-07-16 23:03:15 +0530

    events

---
+++ JS/events-DOM/data1.js
@@ -0,0 +1,41 @@
+function change_btn_colour(){
+    // alert('testcase123')
+    // document.getElementById('abc').innerHTML='gm'
+    document.getElementById('abc').style.backgroundColor="green"
+
+}
+function change_btn_out(){
+    // alert('testcase123')
+    // document.getElementById('abc').innerHTML='gm'
+    document.getElementById('ab').style.backgroundColor="green"
+
+}
+
+function change_btn_click(){
+    // alert('testcase123')
+    // document.getElementById('abc').innerHTML='gm'
+    document.getElementById('cd').style.backgroundColor="green"
+
+}
+
+function change_btn_db(){
+    // alert('testcase123')
+    // document.getElementById('abc').innerHTML='gm'
+    document.getElementById('bc').style.backgroundColor="green"
+
+}
+
+function change_focus(){
+    document.getElementById('de').style.backgroundColor="grey"
+}
+
+function change_blur(){
+    document.getElementById('def')
+}
+
+
+
+    
+
+
+    
