d9b09f209f8cae5523d0b3f1752b5df34771250c
thulasibatchu <bthulasi2552002@gmail.com>
2025-07-30 12:33:55 +0530

    import

---
+++ JS/ES6 features/central.js
@@ -0,0 +1,11 @@
+var tax=18;
+function get_discount(){
+    return 10;
+}
+class Employee{
+  constructor(){
+    console.log("executed")
+  }
+}
+export {tax,get_discount,Employee};
+// export default tax;
---
+++ JS/ES6 features/ka.js
@@ -0,0 +1,5 @@
+import {tax,get_discount,Employee} from "./central.js"
+// import tax from "./central.js"
+console.log(tax)
+console.log(get_discount())
+new Employee();
